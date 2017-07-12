746a2bf6ba5315ea009f06babe5e678518857dbd
Elena Baglaeva <helenbaglaeva@gmail.com>
2017-07-12 22:53:09 +0700

    Optimizations of algo.

--- Percolation.java
+++ Percolation.java
@@ -16,11 +16,15 @@ public class Percolation {
                 blocks[i][j]=0;
             }
         }
-        id = new int[n*n+1];
-        sz = new int[n*n+1];
-        for (int i=0; i< n*n+1; ++i){
+        id = new int[n*n+2];
+        sz = new int[n*n+2];
+        for (int i=0; i< n*n+2; ++i){
             id[i]=i;
             sz[i]=1;
+        }
+        for (int i=1;i<=N;++i) {
+            id[i]=0;
+            id[(N-1)*N+i]=N*N+1;
         }
     }
     private int root(int i) {
@@ -34,9 +38,7 @@ public class Percolation {
         int i=root(p);
         int j=root(q);
         if (i==j) return;
-        if (i<=j) { id[j]=i; sz[i]+=sz[j];}
-        else if (j<=i) { id[i]=j; sz[j]+=sz[i];}
-        else if (sz[i]<sz[j]) { id[i]=j; sz[j]+=sz[i];}
+        if (sz[i]<sz[j]) { id[i]=j; sz[j]+=sz[i];}
         else {id[j]=i; sz[i]+=sz[j];}
     }
     public void open(int row, int col) {
@@ -63,25 +65,19 @@ public class Percolation {
     public boolean isFull(int row, int col){
         if (row<=0 || row>N || col<=0 || col>N)
             throw new java.lang.IllegalArgumentException();
-        return root((row-1)*N+col)<=N;
+        return connected(0,(row-1)*N+col);
     }
     public int numberOfOpenSites(){
         return NumberOfOpenSites;
     }
     public boolean percolates(){
-        for (int i=1; i<=N; ++i) {
-            if (isOpen(N,i)) {
-                if (isFull(N,i))
-                    return true;
-            }
-        }
-        return false;
+        return connected(0,N*N+1);
     }
     
     public static void main(String[] args){
         Percolation pr = new Percolation(3);
         pr.open(2,1);
-        pr.open(3,1);
+        pr.open(2,2);
         pr.open(1,1);
         System.out.println(pr.percolates());
         System.out.println(pr.numberOfOpenSites());
